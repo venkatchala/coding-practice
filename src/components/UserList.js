8eefe5f1798007e67f7f7322807233f87bfaf01f
venkatchala <venkatchala2@gmail.com>
2024-09-30 07:58:05 +0530

    Code updated

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react";
 import { useFetch } from "../utils/useFetch";
 
 const UserList = () => {
-    const { data: users, loading } = useFetch(
+    const { data: users } = useFetch(
       "https://jsonplaceholder.typicode.com/users"
     );
        
-    if (loading) {
-        <h1>Loading...</h1>
-    }
+    // if (loading) {
+    //     <h1>Loading...</h1>
+    // }
         return (
             <ul>
                 {
--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react"
 
 export const useFetch = (url) => {
     const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
           const response = await fetch(url);
           const result = await response.json();
           setData(result);
-          setLoading(false);
+          // setLoading(false);
         };
         fetchData();
     },[url]);
-    return { data, loading }
+    return { data }
 }
